8abd56817af77525bbf47a0885389447e0144258
rishabh-kochar <rishabhkochar58@gmail.com>
2018-04-15 12:57:16 +0530

    Event and Notice Done

--- SmartSocietyWeb/NoticeData.aspx.cs
+++ SmartSocietyWeb/NoticeData.aspx.cs
@@ -1,14 +1,48 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class ViewNotice : System.Web.UI.Page
+public partial class NoticeData : System.Web.UI.Page
 {
+    SSAPIGen.GeneralClient ServiceObjGen = new SSAPIGen.GeneralClient();
+    public static JArray NoticeArr { get; set; }
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            BindData();
+        }
 
+    }
+
+    private void BindData()
+    {
+        NoticeArr = JArray.Parse(ServiceObjGen.ViewAllNotices("0","0",0,0).ToString());
+        rptNotice.DataSource = NoticeArr;
+        rptNotice.DataBind();
+    }
+
+    protected void Unnamed_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+       
+    }
+
+
+
+    protected void btnApply_Click(object sender, EventArgs e)
+    {
+     
+        NoticeArr = JArray.Parse(ServiceObjGen.ViewAllNotices(Convert.ToDateTime(txtStartDate.Text).ToLongDateString(), Convert.ToDateTime(txtEndDate.Text).ToLongDateString(),0,0).ToString());
+        rptNotice.DataSource = (NoticeArr);
+        rptNotice.DataBind();
     }
 }
